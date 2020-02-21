27582f616a06cd21310ad779a7f9c33f072f2087
Alexander Cabrera <56313592+GCalizaya@users.noreply.github.com>
2020-02-21 12:46:08 -0700

    Added SmartDashboard shooter speed.
    Shooter still needs to be updated to work with this new smartDashboard value (in execute and end).

--- robot/Robot.java
+++ robot/Robot.java
@@ -100,11 +100,16 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    
+    SmartDashboard.putNumber("Shooter Speed", 0)
+    
 
+    /*
     SmartDashboard.putNumber("test", 0);
     SmartDashboard.putNumber("Drive kP", 0);
     SmartDashboard.putNumber("Drive kI", 0);
     SmartDashboard.putNumber("Drive kD", 0);
+    */
   }
 
   /**
@@ -115,9 +120,11 @@ public class Robot extends TimedRobot {
     // System.out.println("test value: " + SmartDashboard.getNumber("test", 1));
     SmartDashboard.putNumber("m_speedy", m_robotContainer.m_speedy);
 
+    /*
     VisionConstants.kpDistance = SmartDashboard.getNumber("Drive kP", 0);
     VisionConstants.kiDistance = SmartDashboard.getNumber("Drive kI", 0);
     VisionConstants.kdDistance = SmartDashboard.getNumber("Drive kD", 0);
+    */
   }
 
   @Override
